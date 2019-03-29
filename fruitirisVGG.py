6c9ba9f5319600b1f12ccc8ac92753d159f6b3d6
Wessel Kuijpers <wrkuijpers@outlook.com>
2019-03-29 10:56:52 +0100

    fixed the VGG fruitiris

--- fruitirisVGG.py
+++ fruitirisVGG.py
@@ -3,70 +3,27 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 from keras import backend as K
+from keras.applications.vgg19 import VGG19
 from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
 
 # hyperparameters
 epochs = 5
-batch_size = 32
+batch_size = 1
 # dataset directories
 train_data_dir = 'data/Fruit/train'
 validation_data_dir = 'data/Fruit/test'
 
-# model
+vgg = VGG19(pooling='max')
+vgg.layers.pop()
 model = Sequential()
-# model.add(Conv2D(128, (3, 3), input_shape=(100, 100, 3)))
-# model.add(LeakyReLU(alpha=0.1))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.4))
-
-# model.add(Conv2D(128, (3, 3)))
-# model.add(LeakyReLU(alpha=0.1))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.4))
-
-# model.add(Conv2D(128, (3, 3)))
-# model.add(LeakyReLU(alpha=0.1))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.4))
-
-# model.add(Conv2D(128, (3, 3)))
-# model.add(Dropout(0.1))
-# model.add(LeakyReLU(alpha=0.1))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.4))
-
-# model.add(Flatten())
-# model.add(Dense(128))
-# model.add(LeakyReLU(alpha=0.1))
-# model.add(Dense(11, activation='relu'))
-# model.add(Dense(6, activation='softmax'))
-
-# compile the model
-# model.compile(
-#     loss='categorical_crossentropy',
-#     optimizer='adam',
-#     metrics=['accuracy'])
-
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
+model.add(vgg)
 model.add(Dense(6, activation='softmax'))
-
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # train datagenerator
@@ -90,14 +47,14 @@ test_datagen = ImageDataGenerator(
 # train datastreamer
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
-    target_size=(100, 100),
+    target_size=(224, 224),
     batch_size=batch_size,
     class_mode='categorical')
 
 # validation datastreamer
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
-    target_size=(100, 100),
+    target_size=(224, 224),
     batch_size=batch_size,
     class_mode='categorical')
 
