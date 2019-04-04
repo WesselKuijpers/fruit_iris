00a1e5b33707cfd7ba49c9b71a922629e2c14928
Wessel Kuijpers <wrkuijpers@outlook.com>
2019-04-04 13:56:15 +0200

    fixed the predict file

--- predict.py
+++ predict.py
@@ -7,17 +7,21 @@ import numpy as np
 import tensorflow
 import skimage.io as io
 from skimage.transform import resize
+from keras.models import Sequential
 import tensorflow as tf
 
 # TODO: find a good way to resize an image
 # load a model
-model = load_model('processes/134326032019/fruit360.h5py')
+model = load_model('processes/DenseNet/densenet.h5py')
+seq = Sequential()
+seq.add(model)
+seq.compile(loss='categorical_crossentropy', optimizer='adam')
 # fetch an image
-img = io.imread('img/braeburn2.png', as_gray=False)
+img = io.imread('img/pl5.png', as_gray=False)
 # prepare the image
 img = img.astype('float32')
 img = img / 255
-img = img.reshape(-1, 100, 100, 3)
+img = img.reshape(-1, 224, 224, 3)
 # predict and print
-pred = model.predict_classes(img, verbose=1)
+pred = seq.predict_classes(img, verbose=1)
 print(pred)
